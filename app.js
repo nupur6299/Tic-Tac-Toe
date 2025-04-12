5b00f7a82e002ddf93d570baf1ccf7d808f20fec
nupur6299 <snupur604@gmail.com>
2025-04-12 08:30:58 +0530

    js

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ boxes.forEach((box) => {
 
 const checkWinner = () => {
   for (let pattern of forWin) {
-    console.log("pattern");
+    console.log("pattern[0],pattern[1],pattern[2]");
+    console.log(
+      "boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText"
+    );
   }
 };
